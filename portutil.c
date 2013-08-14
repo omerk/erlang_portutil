1e92149372016f558dd55e4747146c6d4b8697d4
Omer Kilic <omerkilic@gmail.com>
2013-08-14 11:20:16 +0100

    add license headers and update README.

--- portutil.c
+++ portutil.c
@@ -1,3 +1,23 @@
+/**
+ * portutil.c
+ *
+ * This file is part of erlang_portutil <http://github.com/omerk/erlang_portutil>
+ *
+ * Copyright (C) 2013 Omer Kilic <omerkilic@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -6,7 +26,7 @@
 #include <erl_interface.h>
 #include <ei.h>
 
-#include "port_comms.h"
+#include "portutil.h"
 
 
 int
